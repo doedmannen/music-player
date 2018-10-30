5c51ed1b57282179565504eabd6d113d4c79e6f2
Kristoffer Katra <doedmannen@aetmej.se>
2018-10-30 18:38:39 +0100

    Added functions

--- scripts/play.js
+++ scripts/play.js
@@ -1,4 +1,10 @@
 /*
+TODO:
+
+*/
+
+
+/*
   Variablar för användning
 */
 let pause = 1;
@@ -6,35 +12,245 @@ let playing = false;
 let playLoop;
 let track_time_elapsed = 0;
 let currentTrack = 0;
+let currentPlaylist = 0;
 let shuffle = -1;
 let repeat = -1;
+let view = -1;
 
-/*
-  Låtlista
-*/
 
-let tracks = [
-  ["Cannibal Corpse", "Demented Aggression", "194"],
-  ["Cannibal Corpse", "Sarcophagic Frenzy", "222"],
-  ["Cannibal Corpse", "Scourge of Iron", "284"],
-  ["Cannibal Corpse", "Encased in Concrete", "193"],
-  ["Cannibal Corpse", "As Deep as the Knife Will Go", "205"],
-  ["Cannibal Corpse", "Intestinal Crank", "234"],
-  ["Cannibal Corpse", "Followed Home Then Killed", "216"],
-  ["Cannibal Corpse", "The Strangulation Chair", "249"],
-  ["Cannibal Corpse", "Caged... Contorted", "233"],
-  ["Cannibal Corpse", "Crucifier Avenged", "226"],
-  ["Cannibal Corpse", "Rabid", "184"],
-  ["Cannibal Corpse", "Torn Throug", "191"]
-];
+function changeView() {
+  view *= -1;
+  if(view == -1){
+    document.getElementById('playlist_list').style.display = "flex";
+    document.getElementById('track_list').style.display = "none";
+    document.getElementById('back_button').setAttribute('href', "#playlist_list");
+  }else if(view == 1){
+    document.getElementById('playlist_list').style.display = "none";
+    document.getElementById('track_list').style.display = "flex";
+    document.getElementById('back_button').setAttribute('href', "#track_list");
+  }
+}
 
+function changePlaylist(n){
+  currentPlaylist = n;
+  currentTrack = 0;
+  createTracks();
+  changeView();
+
+  if(pause == 1){
+    play_pause();
+  }
+}
+
+
+function updatePlayingTrack(){
+  track_time_elapsed = 0;
+  document.getElementById('player_artist_name').innerHTML = tracks[currentPlaylist][currentTrack][0];
+  document.getElementById('player_track_name').innerHTML = tracks[currentPlaylist][currentTrack][1];
+  let t_min = parseInt(tracks[currentPlaylist][currentTrack][2] / 60);
+  let t_sec = parseInt(tracks[currentPlaylist][currentTrack][2] % 60);
+  if (t_sec < 10) {
+    t_sec = "0" + t_sec;
+  }
+  document.getElementById('player_track_time_total').innerHTML = "" + t_min + ":" + t_sec;
+  document.getElementById('player_track_time_elapsed_bar').style.width = "0%";
+
+  document.getElementById('player_album_cover').setAttribute('src', tracks[currentPlaylist][currentTrack][3]);
+  document.getElementById('player_view').style.backgroundImage = "url(" + tracks[currentPlaylist][currentTrack][3]+ ")";
+  document.getElementById('player_track_time_elapsed').innerHTML = "0:00";
+}
+
+function deleteTracks() {
+  let remover = document.getElementById('track_list_view');
+  if(remover){
+    remover.parentNode.removeChild(remover);
+  }
+}
+
+
+
+
+
+function createTracks() {
+
+  deleteTracks();
+
+  document.getElementById('playlist_name').innerHTML = playlist[currentPlaylist];
+
+  let master;
+
+  master = document.createElement("section");
+  master.setAttribute('class', "flexbox oneflex dir-col scrolling");
+  master.setAttribute('id', "track_list_view");
+
+  document.getElementById('track_list').appendChild(master);
+
+
+  for (let i = 0; i < tracks[currentPlaylist].length; i++) {
+
+    let parent;
+    let child;
+    let grandparent;
+
+    parent = master;
+
+    child = document.createElement("section");
+    child.setAttribute('class', "track flexbox oneflex dir-row align-center");
+    child.setAttribute('id', "view_track_" + i);
+
+    parent.appendChild(child);
+    grandparent = child;
+
+    parent = child;
+
+    child = document.createElement("figure");
+    child.setAttribute('class', "trackpadding albumcover");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("img");
+    child.setAttribute('src', tracks[currentPlaylist][i][3]);
+    child.setAttribute('alt', "album cover");
+    parent.appendChild(child);
+
+    parent = grandparent;
+
+    child = document.createElement("section");
+    child.setAttribute('class', "oneflex flexbox trackpadding dir-col justify-center");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("ul");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("li");
+    child.setAttribute('class', "artist_text");
+    child.innerHTML = tracks[currentPlaylist][i][0];
+    parent.appendChild(child);
+
+    child = document.createElement("li");
+    child.setAttribute('class', "track_text");
+    child.innerHTML = tracks[currentPlaylist][i][1];
+    parent.appendChild(child);
+
+    parent = grandparent;
+
+    child = document.createElement("section");
+    child.setAttribute('class', "trackpadding");
+    child.setAttribute('onclick', "playTrack("+i+")");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("a");
+    child.setAttribute('href', "#");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("i")
+    child.setAttribute('class', "far fa-play-circle");
+    parent.appendChild(child);
+
+
+  }
+
+  updatePlayingTrack();
+}
+
+function createPlaylists() {
+
+  let master;
+
+  master = document.createElement("section");
+  master.setAttribute('class', "flexbox oneflex dir-col scrolling");
+  master.setAttribute('id', "playlist_list_view");
+
+  document.getElementById('playlist_list').appendChild(master);
+
+
+  for (let i = 0; i < playlist.length; i++) {
+
+    let parent;
+    let child;
+    let grandparent;
+
+    parent = master;
+
+    child = document.createElement("section");
+    child.setAttribute('class', "track flexbox oneflex dir-row align-center");
+    child.setAttribute('id', "view_playlist_" + i);
+
+    parent.appendChild(child);
+    grandparent = child;
+
+    parent = child;
+
+    child = document.createElement("figure");
+    child.setAttribute('class', "trackpadding albumcover");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("img");
+    child.setAttribute('src', tracks[i][0][3]);
+    child.setAttribute('alt', "album cover");
+    parent.appendChild(child);
+
+    parent = grandparent;
+
+    child = document.createElement("section");
+    child.setAttribute('class', "oneflex flexbox trackpadding dir-col justify-center");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("ul");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("li");
+    child.setAttribute('class', "artist_text");
+    child.innerHTML = playlist[i];
+    parent.appendChild(child);
+
+
+    parent = grandparent;
+
+    child = document.createElement("section");
+    child.setAttribute('class', "trackpadding");
+    child.setAttribute('onclick', "changePlaylist("+i+")");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("a");
+    child.setAttribute('href', "#");
+    parent.appendChild(child);
+
+    parent = child;
+
+    child = document.createElement("i")
+    child.setAttribute('class', "far fa-play-circle");
+    parent.appendChild(child);
+
+
+  }
+
+  createTracks();
+}
 
 /*
   Översätt nuvarande tid på låt till ett värde mellan 0-100
 */
 
 function mapTo() {
-  return (100 * (track_time_elapsed / tracks[currentTrack][2]));
+  return (100 * (track_time_elapsed / tracks[currentPlaylist][currentTrack][2]));
 }
 
 
@@ -49,7 +265,7 @@ function startLoop() {
   playLoop = setInterval(function() {
     track_time_elapsed++;
 
-    if (track_time_elapsed > tracks[currentTrack][2]) {
+    if (track_time_elapsed > tracks[currentPlaylist][currentTrack][2]) {
       nextTrack();
     }
 
@@ -69,7 +285,7 @@ function startLoop() {
   Shufflar en låt i spellistan
 */
 function doShuffle() {
-  return Math.floor(Math.random() * Math.floor(tracks.length));
+  return Math.floor(Math.random() * Math.floor(tracks[currentPlaylist].length));
 }
 
 
@@ -80,18 +296,18 @@ function doShuffle() {
 */
 
 function nextTrack(n = 0) {
-  if(repeat == 1 && n == 0){
+  if (repeat == 1 && n == 0) {
     // Do nothing
-  } else if(shuffle == 1){
+  } else if (shuffle == 1) {
 
     let newTrack;
-    do{
+    do {
       newTrack = doShuffle();
-    }while(currentTrack == newTrack);
+    } while (currentTrack == newTrack);
 
     currentTrack = newTrack;
 
-  } else if(n != 0){
+  } else if (n != 0) {
 
     currentTrack += n;
 
@@ -101,16 +317,8 @@ function nextTrack(n = 0) {
 
   track_time_elapsed = 0;
 
-  if (currentTrack < tracks.length) {
-    document.getElementById('player_artist_name').innerHTML = tracks[currentTrack][0];
-    document.getElementById('player_track_name').innerHTML = tracks[currentTrack][1];
-    let t_min = parseInt(tracks[currentTrack][2] / 60);
-    let t_sec = parseInt(tracks[currentTrack][2] % 60);
-    if (t_sec < 10) {
-      t_sec = "0" + t_sec;
-    }
-    document.getElementById('player_track_time_total').innerHTML = "" + t_min + ":" + t_sec;
-    document.getElementById('player_track_time_elapsed_bar').style.width = "0%";
+  if (currentTrack < tracks[currentPlaylist].length) {
+    updatePlayingTrack();
   } else {
     currentTrack--;
     play_pause();
@@ -175,20 +383,10 @@ function play_repeat() {
 function playTrack(track_id) {
   stopLoop();
   currentTrack = track_id;
-  document.getElementById('player_artist_name').innerHTML = tracks[track_id][0];
-  document.getElementById('player_track_name').innerHTML = tracks[track_id][1];
-  let t_min = parseInt(tracks[track_id][2] / 60);
-  let t_sec = parseInt(tracks[track_id][2] % 60);
-  if (t_sec < 10) {
-    t_sec = "0" + t_sec;
-  }
-  document.getElementById('player_track_time_total').innerHTML = "" + t_min + ":" + t_sec;
-  document.getElementById('player_track_time_elapsed_bar').style.width = "0%";
+  updatePlayingTrack();
   if (pause == 1) {
     play_pause();
   } else if (!playing) {
     startLoop();
   }
-  track_time_elapsed = 0;
-  document.getElementById('player_track_time_elapsed').innerHTML = "0:00";
 }
