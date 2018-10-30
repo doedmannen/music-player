fc6a15fabff7f56266508cca829e99dab4c3ee14
Kristoffer Katra <doedmannen@aetmej.se>
2018-10-30 18:37:31 +0100

    Split tracks into new file

---
+++ scripts/track_list.js
@@ -0,0 +1,81 @@
+/*
+  Låtlista
+*/
+
+
+let tracks = [];
+
+let playlist = [];
+
+playlist[0] = "Torture";
+tracks[0] = [
+    ["Cannibal Corpse", "Demented Aggression", "194", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Sarcophagic Frenzy", "222", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Scourge of Iron", "284", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Encased in Concrete", "193", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "As Deep as the Knife Will Go", "205", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Intestinal Crank", "234", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Followed Home Then Killed", "216", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "The Strangulation Chair", "249", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Caged... Contorted", "233", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Crucifier Avenged", "226", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Rabid", "184", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"],
+    ["Cannibal Corpse", "Torn Throug", "191", "https://i.ytimg.com/vi/B97v3gn5slQ/maxresdefault.jpg"]
+];
+
+playlist[1] = "Random stuff";
+tracks[1]  = [
+      ["Foster The People", "Pumped Up Kicks", "240", "https://cdn-s3.allmusic.com/release-covers/500/0003/344/0003344477.jpg"],
+      ["Bag Raiders", "Shooting Stars", "236", "https://www.besteveralbums.com/albumart/album_large_12951_4e285d09313c8.jpg"],
+      ["Mike Diva", "Kazoo Kid Trap - Original Mix", "77", "https://i1.sndcdn.com/artworks-000145670296-tigeej-t500x500.jpg"],
+      ["Ween", "Ocean Man", "127", "https://static.stereogum.com/uploads/2017/07/ween-mollusk-1499786118-640x640.jpg"],
+      ["Smash Mouth", "All Star", "200", "https://images-na.ssl-images-amazon.com/images/I/715gWkY5H2L._SL1067_.jpg"]
+];
+//
+// playlist[2] = "";
+// tracks[2] = [
+//     ["", "", "", ""],
+//     ["", "", "", ""],
+//     ["", "", "", ""],
+//     ["", "", "", ""],
+//     ["", "", "", ""],
+//     ["", "", "", ""],
+//     ["", "", "", ""],
+//     ["", "", "", ""]
+// ];
+//
+// playlist[3] = "";
+// tracks[3] = [
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""]
+// ];
+//
+// playlist[4] = "";
+// tracks[4] = [
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""]
+// ];
+//
+// playlist[5] = "";
+// tracks[5] = [
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""],
+//   ["", "", "", ""]
+// ];
